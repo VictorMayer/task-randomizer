3749d69d8bcf83ea4e63ca92397f4bb32a1c00d0
Victor Mayer <victor.mayer@outlook.com>
2023-10-26 01:34:49 -0300

    small bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Menu from './Menu'
 import SelectedTask from './SelectedTask'
 
 function App() {
-  const [selectedTask, setSelectedTask] = useState(JSON.parse(localStorage.getItem('selected')).length > 0 ? JSON.parse(localStorage.getItem('selected'))[0] : false)
+  const [selectedTask, setSelectedTask] = useState(JSON.parse(localStorage.getItem('selected'))?.length > 0 ? JSON.parse(localStorage.getItem('selected'))[0] : false)
   return (
     <>
       { selectedTask 
